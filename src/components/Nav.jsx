fbee986bcdf48451cc469c10d0a47aa08ae89e5b
bbanduraa18 <bbanduraa18@gmail.com>
2024-01-20 11:36:29 +0200

    create hamburger menu for small screens

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,18 +1,25 @@
 import { headerLogo } from "../assets/images";
 import { hamburger } from "../assets/icons";
 import { navLinks } from "../constants";
+import { useState } from "react";
 
 const Nav = () => {
+  let [open, setOpen] = useState(false);
+
   return (
     <header className="padding-x py-8 absolute z-10 w-full">
-      <nav className="flex justify-between items-center max-container">
+      <nav className="lg:flex justify-between items-center max-container">
         <a href="/">
           <img src={headerLogo} alt="Logo" width={130} height={29} />
         </a>
 
-        <ul className="flex-1 flex justify-center items-center gap-16 max-lg:hidden">
+        <div className="lg:hidden absolute right-16 top-8 cursor-pointer" onClick={() => setOpen(!open)}>
+          <img src={hamburger} alt="Hamburger" width={25} height={25} />
+        </div>
+
+        <ul className={`flex-1 lg:flex justify-end items-center gap-16 lg:static absolute lg:w-auto w-full left-0 lg:pl-0 padding-x transition-all duration-500 ease-in ${open ? 'top-[95px] opacity-100' : 'top-[-270px] lg:opacity-100 opacity-0'}`}>
           {navLinks.map((item) => (
-            <li key={item.label}>
+            <li key={item.label} className="lg:my-0 my-8">
               <a
                 href={item.href}
                 className="font-montserrat leading-normal text-lg text-slate-gray"
@@ -22,10 +29,6 @@ const Nav = () => {
             </li>
           ))}
         </ul>
-
-        <div className="hidden max-lg:block">
-            <img src={hamburger} alt="Hamburger" width={25} height={25} />
-        </div>
       </nav>
     </header>
   );
