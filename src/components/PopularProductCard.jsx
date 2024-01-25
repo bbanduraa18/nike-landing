bfc6de9c7a39dc9df37085f0460a800f425866a5
bbanduraa18 <bbanduraa18@gmail.com>
2024-01-25 19:48:35 +0200

    create Subscribe section, refactor property alt property Popular Product Card component

--- src/components/PopularProductCard.jsx
+++ src/components/PopularProductCard.jsx
@@ -5,7 +5,7 @@ const PopularProductCard = ({ imgURL, name, price }) => {
     <div className="flex flex-1 flex-col w-full max-sm:w-full">
       <img src={imgURL} alt="name" className="w-[280px] h-[280px]" />
       <div className="mt-8 flex justify-start gap-2.5">
-        <img src={star} alt="raitinf" width={24} height={24} />
+        <img src={star} alt="star" width={24} height={24} />
         <p className="font-montserrat text-xl leading-normal text-slate-gray">
           (4.5)
         </p>
