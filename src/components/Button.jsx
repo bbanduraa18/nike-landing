1b119a6779cb89270f7cfc748b3b9a0b0c9e6a97
bbanduraa18 <bbanduraa18@gmail.com>
2024-01-25 18:26:13 +0200

    create Super Quality section, refactor Hero and Button components

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,7 +3,11 @@ const Button = ({ label, iconURL, alt }) => {
     <button className="flex justify-center items-center gap-2 px-7 py-4 font-montserrat text-lg text-white leading-none bg-coral-red border border-coral-red rounded-full">
       {label}
 
-      <img src={iconURL} alt={alt} className="ml-2 rounded-full w-5 h-5" />
+      {iconURL && <img
+        src={iconURL}
+        alt={alt}
+        className="ml-2 rounded-full w-5 h-5"
+      />}
     </button>
   );
 };
--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -22,7 +22,7 @@ const Hero = () => {
           <br />
           <span className="text-coral-red inline-block mt-3">Nike</span> Shoes
         </h1>
-        <p className="font-motserrat text-slate-gray text-lg leading-8 mt-6 mb-14 sm:max-w-sm">
+        <p className="font-montserrat text-slate-gray text-lg leading-8 mt-6 mb-14 sm:max-w-sm">
           Discover stylish Nike arrivals, quality comfort, and innovation for
           your active life.
         </p>
